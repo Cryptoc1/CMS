bb6ec27fd61f1e020b9fa72cf96770c04f8ec60b
Samuel Steele <steelesam72@gmail.com>
2015-01-26 01:28:30 -0500

    initial commit

---
+++ CMS.py
@@ -0,0 +1,48 @@
+import MySQLdb as mdb
+import sys
+
+version = "0.0.0b"
+error_message = "There was an error when attempting to process your request. System returned: "
+def get_all_posts():
+    try:
+        con = mdb.connect("localhost", "test", "test123", "CMS")
+
+        cur = con.cursor()
+        cur.execute("SELECT * FROM Posts")
+
+        res = cur.fetchall()
+
+        return res
+
+    except mdb.Error, e:
+        print error_message . e
+        sys.exit(1)
+
+    finally:
+        if con:
+            con.close()
+
+def new_post():
+    try:
+        con = mdb.connect("localhost", "test", "test123", "CMS")
+
+        cur = con.cursor()
+        cur.execute("")
+
+        ''' 
+        if POST successful:
+            print success message
+        elif:
+            print error message
+        '''
+
+    except mdb.Error, e:
+        print error_message . e
+        sys.exit(1)
+
+    finally:
+        if con:
+            con.close()
+
+def version():
+    print version
---
+++ README.md
@@ -0,0 +1,8 @@
+# CMS
+A 'Content Management System' written in Python
+
+## What is the purpose of CMS?
+CMS was a project I created when learning MySQL and building data systems
+
+## What stage is development in?
+CMS is in very early "development".

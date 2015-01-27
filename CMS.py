159f317b82655127b8cfe1d8c676fed9a26fe3d0
Samuel Steele <steelesam72@gmail.com>
2015-01-26 21:30:24 -0500

    Update to file

--- CMS.py
+++ CMS.py
@@ -3,7 +3,7 @@ import MySQLdb as mdb
 import sys
 
 ver = "0.0.0b"
-error_message = "There was an error when attempting to process your request. System returned: "
+error_message = "There was an error when attempting to process your request. System returned:\n"
 
 class CMS:
     def __init__(self, hostname, username, password, db):
@@ -36,66 +36,148 @@ class CMS:
     def get_db(self):
         return self.db
 
+    def get_entry_count(self):
+        try:
+            con = mdb.connect(self.hostname, self.username, self.password, self.db)
+
+            cur = con.cursor()
+            cur.execute("SELECT COUNT(*) FROM Posts;")
+
+            res = cur.fetchone()
+
+            return res[0]
+
+        except mdb.Error, e:
+            print error_message + str(e)
+            sys.exit(1)
+        finally:
+            if con:
+                con.close()
+
     def get_all_posts(self):
         try:
             con = mdb.connect(self.hostname, self.username, self.password, self.db)
 
             cur = con.cursor()
-            cur.execute("SELECT * FROM Posts")
+            cur.execute("SELECT * FROM Posts;")
 
-            res = cur.fetchone()
+            res = cur.fetchall()
+            posts =[]
+            for i in res:
+                p = {}
+                p["pid"] = i[0]
+                p["title"] = i[1]
+                p["author"] = i[2]
+                p["content"] = i[3]
+                posts.append(p)
 
-            return res
+            return posts
 
         except mdb.Error, e:
-            print error_message + e
+            print error_message + str(e)
             sys.exit(1)
-
         finally:
             if con:
                 con.close()
-
-    def get_post(self, pid, title, author):
+    
+    def get_post_by_id(self, pid):
         if pid:
-            req = "SELECT * FROM Posts WHERE pid=" + pid
-        elif title:
-            req = "SELECT * FROM Posts WHERE title=" + title
-        elif author:
-            req = "SELECT * FROM Posts WHERE author=" + author
+            req = "SELECT * FROM Posts WHERE pid=\"" + str(pid) + "\";"
+            try:
+                con = mdb.connect(self.hostname,  self.username, self.password, self.db)
+                
+                cur = con.cursor()
+                cur.execute(req)
+                res = cur.fetchone()
 
-        try:
-            con = mdb.connect(self.hostname,  self.username, self.password, self.db)
-        
-        except mdb.Error, e:
-            print error_message + e
+                if res == None:
+                    print error_message + "No results found, reponse from server: "
+                else:
+                    post = {}
+                    post["pid"] = res[0]
+                    post["title"] = res[1]
+                    post["author"] = res[2]
+                    post["content"] = res[3]
 
-        finally:
-            if con:
-                con.close()
-    def new_post():
-        try:
-            con = mdb.connect(self.hostname, self.username, self.password, self.db)
+                return post
 
-            cur = con.cursor()
-            cur.execute("")
-
-            ''' 
-            if POST successful:
-                print success message
-            elif:
-                print error message
-            '''
-
-        except mdb.Error, e:
-            print error_message + e
+            except mdb.Error, e:
+                print error_message + str(e)
+                sys.exit(1)
+            finally:
+                if con:
+                    con.close()
+        else:
+            print error_message + "No Post ID (pid) specified."
             sys.exit(1)
 
-        finally:
-            if con:
-                con.close()
+    def get_posts_by_title(self, title):
+        if title:
+            req = "SELECT * FROM Posts WHERE title=\"" + title + "\";"
+            try:
+                con = medb.connect(self.hostname, self.username, self.password, self.db)
+
+                cur = con.cursor()
+                cur.execute(req)
+                res = cur.fetchone()
+
+            except mdb.Error, e:
+                print error_message + str(e)
+                sys.exit(1)
+            finally:
+                if con:
+                    con.close()
+        else:
+            print error_message + "No Post Title (title) specified."
+            sys.exit(1)
+
+    def get_posts_by_author(self, author):
+        if author:
+            req = "SELECT * FROM Posts WHERE author=\"" + author + "\";"
+            try:
+                con = mdb.connect(self.hostname, self.username, slef.password, self.db)
+
+                cur = con.cursor()
+                cur.execute(req)
+
+                res = cur.fetchone()
+
+            except mdb.Error, e:
+                print error_message + str(e)
+                sys.exit(1)
+            finally:
+                if con:
+                    con.close()
+        else:
+            print error_message + "No Post Author (author) specified."
+            sys.exit(1)
+
+    def new_post(title, author, content):
+        if title and author and content:
+            req = "INSERT INTO Posts (title, author, content) VALUES ("  + title + ", " + author + ", " + content + ");"
+            try:
+                con = mdb.connect(self.hostname, self.username, self.password, self.db)
+
+                cur = con.cursor()
+                cur.execute(req)
+
+                ''' 
+                if POST successful:
+                    print success message
+                elif:
+                    print error message
+                '''
+
+            except mdb.Error, e:
+                print error_message + str(e)
+                sys.exit(1)
+
+            finally:
+                if con:
+                    con.close()
+        else:
+            print error_message + "Proper arguments not fulfilled."
+            sys.exit(1)
 
     def version():
         print ver
-
-class Post(str):
-    # TODO : Construct Object from string returned from Database

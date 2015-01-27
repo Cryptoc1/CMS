ad20c9a19763d448f485b5ce5e68bf62296d544c
Samuel Steele <steelesam72@gmail.com>
2015-01-27 10:01:23 -0500

    Update to file

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 from CMS import CMS
 
-manager = CMS(hostname="localhost", username="test", password="test123", db="")
-manager.set_db("CMStest")
+manager = CMS(hostname="localhost", username="test", password="test123", db="CMS")
 
-# print manager."method"
+# manager.some_method()
